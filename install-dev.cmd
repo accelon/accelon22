0ba2173ad5f075276637bc80aafaf93ef2782513
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2023-01-18 17:45:36 +0800

    tested install-dev

--- install-dev.cmd
+++ install-dev.cmd
@@ -1,14 +1,52 @@
+@echo off
+@REM 目錄建議
+@REM 第一次須安裝 esbuild ，之後可以 注解掉，以加快安裝速度
+cmd/c npm i -g esbuild
+
+@REM 上一層目錄 要有 node_modules ，以方便其他 repo 共用
 cmd/c npm i
 cd ..
-mkdir node_modules
+if not exist "node_module" ( 
+    mkdir node_modules
+)
+
 cmd/c npm i diff
-     
-git clone https://github.com/accelon/ptk
-git clone https://github.com/accelon/lossless-simplified-chinese
-git clone https://github.com/dhamma/provident-pali
-     
+
+
+@REM 安裝 ptk
+if not exist "ptk" (
+    git clone https://github.com/accelon/ptk
+)
+@REM 安裝 ptk cli ，讓 ptk 變成通用指令
+cmd/c npm i -g ./ptk
+
+
+@REM accelon22 還需要這兩個包
+if not exist "lossless-simplified-chinese" (
+    git clone https://github.com/accelon/lossless-simplified-chinese
+)
+
+if not exist "provident-pali" (
+    git clone https://github.com/dhamma/provident-pali
+)
+
+@REM 因為 node_modules 下不能有被 git 管理的目錄，故安裝在同層，然後做目錄連結。
 cd node_modules
-mklink/j ptk ..\ptk
-mklink/j lossless-simplified-chinese ..\lossless-simplified-chinese
-mklink/j provident-pali ..\provident-pali
-cd ..
+
+if not exist "ptk" (
+    mklink/j ptk ..\ptk
+)
+
+if not exist "lossless-simplified-chinese" (
+    mklink/j lossless-simplified-chinese ..\lossless-simplified-chinese
+)
+if not exist "provident-pali" (
+    mklink/j provident-pali ..\provident-pali
+)
+
+
+cd ptk
+@REM 造出 nodebundle.cjs
+cmd/c build-cjs.cmd
+
+cd ../accelon22
